7695f85ad3f2fde3fd16a6e4e045cc32939f3c82
ASJ THILAK <78794112+ASJTHILAK@users.noreply.github.com>
2021-02-09 16:21:04 +0530

    CREATED SWAP.c
    Before and After swapping.

---
+++ SWAP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int X = 33, Y = 18; // Intializing two variables 'X' and 'Y'
+    printf("Before Swapping: X = %d, Y = %d\n", X, Y); // Before swapping
+ 	  // Code to swap 'X' and 'Y' without using a temporary variable
+    X = X + Y; // X now becomes 51
+    Y = X - Y; // Y becomes 33
+    X = X - Y; // X becomes 18
+	  printf("After Swapping: X = %d, Y = %d", X, Y); // After swapping
+   	return 0;
+}
